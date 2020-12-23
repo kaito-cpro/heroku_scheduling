8aca8d100abef2ead61b3e5d991b90c03371c750
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-24 03:16:45 +0900

    update

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
     <head>
         <title>調整さん 拡張機能(仮)</title>
-        <meta charset="utf-8">
+        <meta charset="utf-8" name="viewport"
+              content="width=320,
+                       height=480,
+                       initial-scale=1.0,
+                       minimum-scale=1.0,
+                       maximum-scale=2.0,
+                       user-scalable=yes" />
         <link rel="stylesheet" href="style.css" />
     </head>
     <body onload="brython()">
@@ -13,7 +19,7 @@
                 <input class="input_form" type="text" name="url">
             </div>
             <div class="btn-area">
-                <input type="submit" value="送信">
+                <input type="submit" value="読み込む">
             </div>
         </form>
     </body>
--- next.php
+++ next.php
@@ -1,7 +1,13 @@
 <html>
     <head>
         <title>調整さん 拡張機能(仮)</title>
-        <meta charset="utf-8">
+        <meta charset="utf-8" name="viewport"
+              content="width=320,
+                       height=480,
+                       initial-scale=1.0,
+                       minimum-scale=1.0,
+                       maximum-scale=2.0,
+                       user-scalable=yes" />
         <link rel="stylesheet" href="style.css" />
     </head>
     <body onload="brython()">
@@ -16,5 +22,7 @@
                 echo $o;
             }
         ?>
+       
+        <input type="button" onclick="history.back()" value="戻る">
     </body>
 </html>
