a23d4d94c221555e580b889fc025eadd06ead56c
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-23 23:36:28 +0900

    update

--- next.php
+++ next.php
@@ -7,6 +7,7 @@
         <h1>調整さん 拡張機能(仮)</h1>
         
         <?php
+            exec("export LANG=ja_JP.UTF-8");
             $command="python exec_from_php.py ".$_POST['url'];
             exec($command,$output,$rtn);
             print_r($rtn);
