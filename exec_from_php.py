a4d678159ca19d455b3deea5a7416fab7d5998a2
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-24 03:42:15 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -60,7 +60,7 @@ for i in range(len(table)):
     for j in range(3):
         html_body += f'''
                 <td>
-                    <label for="trigger{3 * i + j}" class="open_btn">{table[i].count(j)}人</label>
+                    <label for="trigger{3 * i + j}" class="open_btn">{str(table[i].count(j)).rjust(2)}人</label>
                     <div class="popup_wrap">
                     <input id="trigger{3 * i + j}" type="checkbox">
                     <div class="popup_overlay">
