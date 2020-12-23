3e9eeaef67358cbb71dcc76883754f606470d150
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-23 23:28:54 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -4,5 +4,42 @@ from bs4 import BeautifulSoup
 
 url = sys.argv[1]
 
-print(url)
-print("hoge")
+html = requests.get(url)
+
+soup = BeautifulSoup(html.text, "lxml")
+
+dates = []  # 日時
+info = []  # 名前と出欠
+
+for e in soup.find_all('td', style="text-align:left;border-bottom:1px solid #ddd"):
+    dates.append(e.text)
+    
+for e in soup.find_all('td', style="border-bottom:1px solid #ddd"):
+    info.append(e.text)
+
+dates.remove('日程')
+dates.remove('コメント')
+
+members_num = len(info) // (1 + len(dates))  # 人数
+
+names = [info[i].strip() for i in range(members_num)]  # 名前
+attends = [info[i + members_num] for i in range(len(info) - members_num)]  # 出欠
+
+# print(dates)
+# print(names)
+# print(attends)
+
+table = [[None for _ in range(len(names))] for _ in range(len(dates))]
+for i in range(len(table)):
+    for j in range(len(table[i])):
+        if attends[i * members_num + j] == '○':
+            table[i][j] = 1
+        elif attends[i * members_num + j] == '△':
+            table[i][j] = 0
+        elif attends[i * members_num + j] == '×':
+            table[i][j] = -1
+
+# print(table)
+
+for date in dates:
+    print(date)
