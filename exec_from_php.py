793a291a18d2264490923f77e47e20906072a568
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-23 14:43:27 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -1,14 +1,8 @@
-'''
 import sys
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
 url = sys.argv[1]
 
 print(url)
-print("hoge")
-'''
-
-print("hoge")
-print("hoge")
 print("hoge")
