d1f43e94655d479cccd7c9547cba2794d8d1d1d6
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-24 13:33:53 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -85,7 +85,7 @@ for i in range(len(table)):
                 names_disp.append(names[k])
         for name in names_disp:
             html_body += '<p>' + name + '</p>'
-        html_body += f'({table[i],count(j)}人)'
+        html_body += f'<td align="right">({table[i],count(j)}人)</td>'
             
         html_body += '''
                     </div>
