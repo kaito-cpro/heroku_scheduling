3dc61fed668114e82b95e9de2a6469962d1f1781
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-25 21:39:36 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -74,11 +74,11 @@ for i in range(len(table)):
                     <label for="trigger{3 * i + j}" class="open_btn">{table[i].count(j)}人</label>
                     <div class="popup_wrap">
                     <input id="trigger{3 * i + j}" type="checkbox">
+                    <div class="popup_overlay">
+                    <label for="trigger{3 * i + j}" class="popup_trigger"></label>
                     <div class="back_img">
                         <img src="img/{symbol_disp[j]}2.png">
                     </div>
-                    <div class="popup_overlay">
-                    <label for="trigger{3 * i + j}" class="popup_trigger"></label>
                     <div class="popup_content">
                     <label for="trigger{3 * i + j}" class="close_btn">&times;</label>
                     <h2>&nbsp;{dates[i]} </h2>'''
