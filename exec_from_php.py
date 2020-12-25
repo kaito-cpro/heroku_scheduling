840c173af3747ebdb853c9e81cb4e5fda52d1fb2
kaito-cpro <blue7-go0112@docomo.ne.jp>
2020-12-25 22:25:24 +0900

    update

--- exec_from_php.py
+++ exec_from_php.py
@@ -41,15 +41,15 @@ for i in range(len(table)):
 
 # print(table)
 
-# prev_day = None
+prev_day = -1
 def decide_color(date):
-    # day = date.split('/')[1].split('(')[0]
-    # if day == prev_day:
-        # prev_day = day
-    return '#ffe6e6'
-    # else:
-        # prev_day = day
-        # return '#c2f3e7'
+    day = date.split('/')[1].split('(')[0]
+    if day == prev_day:
+        prev_day = day
+        return '#ffe6e6'
+    else:
+        prev_day = day
+        return '#c2f3e7'
 
 html_body = f'''
     <table>
@@ -67,7 +67,7 @@ html_body = f'''
 for i in range(len(table)):
     html_body += f'''
         <tr>
-            <td bgcolor={decide_color(dates[i])}>{dates[i]}</td>'''
+            <td bgcolor="{decide_color(dates[i])}">{dates[i]}</td>'''
     for j in range(3):
         html_body += f'''
                 <td>
