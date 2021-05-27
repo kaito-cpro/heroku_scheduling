90745d214f6e1ec58ffd3f15004af4c216874648
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-05-27 21:23:11 +0900

    change button design

--- next.php
+++ next.php
@@ -9,7 +9,7 @@
                        minimum-scale=1.0,
                        maximum-scale=2.0,
                        user-scalable=yes" />
-        <link rel="stylesheet" href="style.css?202105272041" />
+        <link rel="stylesheet" href="style.css?202105272122" />
     </head>
     <body onload="brython()"> 
         <h1>調整さん 拡張機能</h1>
