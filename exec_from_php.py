ea067042c9bcf04da6f964d030424d6c1b476837
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-05-27 15:36:20 +0900

    fix bug

--- exec_from_php.py
+++ exec_from_php.py
@@ -125,6 +125,7 @@ html_body += '</table>'
 
 html_body += '''
     <div class="popup_wrap">
+        <label for="trigger_maintenance" class="open_btn">test</label>
         <div class="popup_overlay">
             <input id="trigger_maintenance" type="checkbox" checked="checked>
             <label for="trigger_maintenance" class="popup_trigger"></label>
