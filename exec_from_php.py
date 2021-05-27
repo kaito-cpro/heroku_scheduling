4826a32d0cab08157667299683cfca88c06c23fc
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-05-27 16:04:17 +0900

    fix bug

--- exec_from_php.py
+++ exec_from_php.py
@@ -124,17 +124,19 @@ for i in range(len(table)):
 html_body += '</table>'
 
 html_body += '''
-    <label for="trigger_maintenance" class="open_btn">test</label>
-    <div class="popup_wrap">
-        <input id="trigger_maintenance" type="checkbox" checked="checked>
-        <div class="popup_overlay">
-            <label for="trigger_maintenance" class="popup_trigger"></label>
-            <div class="popup_content">
-                <div class="text_title">メンテナンスのお知らせ</div>
-                <p>ただいま Developer によるメンテナンス中です。</p>
-                <p>表示内容に少し支障が生じる場合がありますが、その間も本ツールを利用すること自体は可能です。</p>
-                <p>なお、メンテナンスは数時間で終わる予定です。</p>
-                <label for="trigger_maintenance" class="close_btn"><img src="img/close_btn.png" width=30px></label>
+    <div class="box">
+        <label for="trigger_maintenance" class="open_btn">test</label>
+        <div class="popup_wrap">
+            <input id="trigger_maintenance" type="checkbox" checked="checked>
+            <div class="popup_overlay">
+                <label for="trigger_maintenance" class="popup_trigger"></label>
+                <div class="popup_content">
+                    <div class="text_title">メンテナンスのお知らせ</div>
+                    <p>ただいま Developer によるメンテナンス中です。</p>
+                    <p>表示内容に少し支障が生じる場合がありますが、その間も本ツールを利用すること自体は可能です。</p>
+                    <p>なお、メンテナンスは数時間で終わる予定です。</p>
+                    <label for="trigger_maintenance" class="close_btn"><img src="img/close_btn.png" width=30px></label>
+                </div>
             </div>
         </div>
     </div>'''
