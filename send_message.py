8f78f33676c73f9966b612370238c996b26cd608
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-05 21:15:04 +0900

    update

--- send_message.py
+++ send_message.py
@@ -2,7 +2,10 @@ import sys
 import requests
 import os
 
-line_notify_token = os.environ["LINE_TOKEN"]  # LINE Notifyのアクセス(自分が発行したトークンへ変更)
+line_notify_token = os.environ["LINE_TOKEN"]  # LINE Notify のアクセストークン
+# LINE Notify のアクセストークンの環境変数の設定の仕方
+# $ heroku config:set LINE_TOKEN=******* --app heroku-scheduling
+
 line_notify_api = 'https://notify-api.line.me/api/notify'  # LINE Notify の API アドレス
 
 message = ''.join(sys.argv[1:])
