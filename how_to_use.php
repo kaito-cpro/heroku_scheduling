072c5ea379be97bbd1af7530ad51065d5c62a258
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-06 02:51:18 +0900

    update

--- how_to_use.php
+++ how_to_use.php
@@ -17,7 +17,7 @@
                        minimum-scale=1.0,
                        maximum-scale=2.0,
                        user-scalable=yes" />
-        <link rel="stylesheet" href="style.css?202106060149" />
+        <link rel="stylesheet" href="style.css?202106060150" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.8.8/brython.js" integrity="sha256-rA89wPrTJJQFWJaZveKW8jpdmC3t5F9rRkPyBjz8G04=" crossorigin="anonymous"></script>
     </head>
     <body onload="brython()"> 
@@ -28,17 +28,17 @@
                 <label class="label">調整さん拡張機能の使い方</label>
             </div>
         </form>
-        <p><div class="dot"></div>まず、URL 欄に「調整さん」のイベント URL を入力します。トップページの「<a href="https://chouseisan.com/">調整さん</a>」のリンクを踏んで、イベント履歴から URL をコピペするのがオススメです。</p>
-        <p><div class="dot"></div>次に、読み込みボタンを押すと出欠表が表示されます。人数のマスをタッチするとメンバーの一覧が表示されます。</p>
-        <p><div class="dot"></div>サイドメニューを開き、メンバーの名前の左にあるチェックボタンを押すと、そのメンバーがハイライト表示されます。複数人をチェックすることも可能です。</p>
-        <p><div class="dot"></div>サイドメニューのお問い合わせフォームからは、開発者(Developer: Kai)へメッセージを送ることができます。バグの報告やご要望などがあれば気軽に送ってください。</p>
+        <p> <div class="dot"></div>まず、URL 欄に「調整さん」のイベント URL を入力します。トップページの「<a href="https://chouseisan.com/">調整さん</a>」のリンクへ行き、イベント履歴から URL をコピペするのがオススメです。</p>
+        <p> <div class="dot"></div>次に、読み込みボタンを押すと出欠表が表示されます。人数のマスをタップするとメンバーの一覧が表示されます。</p>
+        <p> <div class="dot"></div>サイドメニューを開き、メンバーの名前の左にあるチェックボタンを押すと、そのメンバーがハイライトで表示されます。複数人をチェックすることも可能です。</p>
+        <p> <div class="dot"></div>サイドメニューのお問い合わせフォームからは、開発者(Developer: Kai)へメッセージを送ることができます。バグの報告やご要望などがあれば気軽に送ってください。</p>
         
         <div class="space"></div>
         
-        <p><span class="under">注意点</span></p>
-        <p><div class="dot"></div>本ツールは、トップページの読み込みボタンを押すことによって最新の「調整さん」の出欠表データを取得しています。よって、ブラウザ等で本ツールを開いたまま長時間が経過した場合にはデータが最新ではありませんので、必ずトップページから読み込みボタンを押して最新のデータに更新してご利用ください。</p>
-        <p><div class="dot"></div>本ツールは <a href="https://jp.heroku.com/what">heroku</a> の無料プラットフォームで動作しており、毎月 550 時間までサーバを利用できます。ほぼ大丈夫ですが、この 550 時間を超えてしまった場合は一時的に本ツールを利用できなくなる場合がありますのでご了承ください。</p>
-        <p><div class="dot"></div>本ツールは無料サーバを利用しているため、初回アクセス時にサーバを立ち上げる時間がかかります。よって、初回アクセス時にうまく接続が安定しない場合はリロードするとうまく接続されます。</p>
+        <p><span class="under"> 注意点 </span></p>
+        <p> <div class="dot"></div>本ツールは、トップページの読み込みボタンを押すことによって最新の「調整さん」の出欠表データを取得しています。よって、ブラウザ等で本ツールを開いたまま長時間が経過した場合にはデータが最新ではありませんので、必ずトップページから読み込みボタンを押して最新のデータに更新してご利用ください。</p>
+        <p> <div class="dot"></div>本ツールは <a href="https://jp.heroku.com/what">heroku</a> の無料プラットフォームで動作しており、毎月 550 時間までサーバを利用できます。ほぼ大丈夫ですが、この 550 時間を超えてしまった場合は一時的に本ツールを利用できなくなる場合がありますのでご了承ください。</p>
+        <p> <div class="dot"></div>本ツールは無料サーバを利用しているため、初回アクセス時にサーバを立ち上げる時間がかかります。よって、初回アクセス時にうまく接続が安定しない場合はリロードするとうまく接続されます。</p>
         
         <input type="checkbox" id="navTgl">
         <label for="navTgl" class="open"><span></span></label>
@@ -53,6 +53,7 @@
             </ul>
         </nav>
         
+        <div class="space"></div>
         <input type="button" onclick="history.back()" value="戻る">
     </body>
 </html>
