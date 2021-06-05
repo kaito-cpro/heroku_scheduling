c7f1d9b7969788d29ef7a6b1f5f7e270ceaf4b97
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-05 15:17:07 +0900

    maintenance

--- history_table.js
+++ history_table.js
@@ -1,14 +1,12 @@
 function make_history_table() {
-    alert("called alert");
-    console.log("called");
     var table = document.getElementsByClassName('history-table')[0];
     var cookies = document.cookie.split("; ");
     for (let i = 0; i < cookies.length; ++i) {
         if (cookies[i].indexOf("event_url_history") === 0) {
+            console.log(cookies[i]);
             var newRow = table.insertRow();
             newRow.content = cookies[i];
             break;
         }
     }
-    console.log("called");
 }
