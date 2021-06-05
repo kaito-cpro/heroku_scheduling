28058cc4bf72a38853ca42e4856482f8e5dc5c45
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-05 12:11:11 +0900

    maintenance

--- index.php
+++ index.php
@@ -37,6 +37,13 @@
                 <input class="input_form" type="text" name="url">
             </div>
             
+            <script>
+                document.write('履歴');
+                for (let i = 0; i < 3, ++i) {
+                    document.write('hoge');
+                }
+            </script>
+            
             <div class="btn-area">
                 <input type="submit" value="読み込む" onclick="load();">
             </div>
@@ -53,6 +60,21 @@
         
         <div id="loading" style="display:none;">
             <div class="loadingMsg"></div>
+        </div>
+        
+        <div class="popup_wrap">
+            <input id="trigger_maintenance" type="checkbox" checked="checked">
+            <div class="popup_overlay">
+                <label for="trigger_maintenance" class="popup_trigger"></label>
+                <div class="popup_content">
+                    <div class="text_title">メンテナンスのお知らせ</div>
+                    <p>ただ今、Developer によるメンテナンス中です。</p>
+                    <p>メンテナンス中も本ツールを利用することは可能ですが、一部の表示等に乱れが生じる場合があります。</p>
+                    <p>なお、メンテナンスは数時間ほどで終了する予定です。</p>
+                    <div class="space"></div>
+                    <label for="trigger_maintenance" class="close_btn"><img src="img/close_btn.png" width=30px></label>
+                </div>
+            </div>
         </div>
     </body>
 </html>
