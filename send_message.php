27c2678e6b235a0cd8d2eeacdf1dd820735b3b89
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-12 12:03:49 +0900

    fix bug

--- send_message.php
+++ send_message.php
@@ -26,8 +26,7 @@
         
         <?php
             exec("export LANG=ja_JP.UTF-8");
-            echo $_POST['message'];
-            $command="python send_message.py ".$_POST['message'];
+            $command="python send_message.py "."\"."$_POST['message']."\"";
             exec($command,$output,$rtn);           
             foreach ($output as $o) {
                 echo $o;
