fad09c854d66e561555bcf0fe31692e9b34841e9
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-06-14 17:25:18 +0900

    fix bug

--- loading.js
+++ loading.js
@@ -18,14 +18,15 @@ function post(path, params, method='post') {
     form.submit();
 }
   
-function load(id) {
+function load(id, page) {
+    console.log("called");
     var loader = document.getElementById('loading');
     setTimeout(function(){
         loader.style.display = '';
     }, 100);
     setTimeout(endLoader, 30000);
     var value = document.getElementById(id).value;
-    post("table.php", {val: value});
+    post(page, {val: value});
 }
 
 function endLoader(){
