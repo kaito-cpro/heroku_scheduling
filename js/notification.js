320c9b85a0fe2cae1534bc3ff23fe9658c30f18d
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-07-07 04:18:07 +0900

    fix bug

--- js/notification.js
+++ js/notification.js
@@ -1,6 +1,6 @@
 function open_notifications() {
     // var trigger = document.getElementById("trigger_notification");
-    var trigger = document.getElementById("trigger_popuppopup_survey");  // 暫定
-    trigger.checked = "checked";
+    var popup_survey = document.getElementById("popup_survey");  // 暫定
+    popup_survey.style.display = "";
     document.cookie = "notification_ver=" + "0" +  "; max-age=5184000";  // 暫定
 }
