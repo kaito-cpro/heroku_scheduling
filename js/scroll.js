65a409693cd40aa3ee735fba5d237ce16a2795ed
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-07-19 18:33:30 +0900

    update

--- js/scroll.js
+++ js/scroll.js
@@ -29,14 +29,3 @@ $(function() {
         else document.body.style.overflowY = "";
     }); 
 });
-
-// 1.関数の定義
-function setHeight() {
-    alert("resized");
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-// 2.初期化
-setHeight();
-// 3.ブラウザのサイズが変更された時・画面の向きを変えた時に再計算する
-window.addEventListener('resize', setHeight);
