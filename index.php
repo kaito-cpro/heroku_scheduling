81c0507537decdd34bde0c1f1be790751319ed58
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-07-19 21:02:22 +0900

    update

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                        maximum-scale=2.0,
                        user-scalable=yes" />
         <meta name="keywords" content="調整さん,拡張機能" />
-        <link rel="stylesheet" href="style.css?202107191706" />
+        <link rel="stylesheet" href="style.css?202107191707" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.8.8/brython.js" integrity="sha256-rA89wPrTJJQFWJaZveKW8jpdmC3t5F9rRkPyBjz8G04=" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
@@ -166,9 +166,9 @@
                 // check_popup_ver(1.7, 'popup_survey');
             </script>
             
-            <!-- <script type="text/javascript">
+            <script type="text/javascript">
                 popup_maintenance("popup_maintenance", true);
-            </script> -->
+            </script>
             
             <!-- check notification-version and developer-mode -->
             <script type="text/javascript">
